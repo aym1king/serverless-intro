138ae3e8a53251c5e5248d1572bb6a96d85925ba
aym1king <49128030+aym1king@users.noreply.github.com>
2023-11-23 20:18:31 +0100

    Update wine-feature-pipeline-daily.py

--- wine/wine-feature-pipeline-daily.py
+++ wine/wine-feature-pipeline-daily.py
@@ -4,7 +4,7 @@ import modal
 LOCAL=True
 
 if LOCAL == False:
-   stub = modal.Stub("iris_daily")
+   stub = modal.Stub("wine_daily")
    image = modal.Image.debian_slim().pip_install(["hopsworks"]) 
 
    @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("id2223"))
@@ -12,47 +12,56 @@ if LOCAL == False:
        g()
 
 
-def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_width_min, 
-                    petal_len_max, petal_len_min, petal_width_max, petal_width_min):
+def generate_wine(name, fixed_acidity_max, fixed_acidity_min, volatile_acidity_max, volatile_acidity_min,
+                    citric_acid_max, citric_acid_min, residual_sugar_max, residual_sugar_min,
+                    chlorides_max, chlorides_min, free_sulfur_dioxide_max, free_sulfur_dioxide_min,
+                    total_sulfur_dioxide_max, total_sulfur_dioxide_min, density_max, density_min,
+                    pH_max, pH_min, sulphates_max, sulphates_min, alcohol_max, alcohol_min):
     """
-    Returns a single iris flower as a single row in a DataFrame
+    Returns a single wine as a single row in a DataFrame
     """
     import pandas as pd
     import random
 
-    df = pd.DataFrame({ "sepal_length": [random.uniform(sepal_len_max, sepal_len_min)],
-                       "sepal_width": [random.uniform(sepal_width_max, sepal_width_min)],
-                       "petal_length": [random.uniform(petal_len_max, petal_len_min)],
-                       "petal_width": [random.uniform(petal_width_max, petal_width_min)]
-                      })
-    df['variety'] = name
+    df = pd.DataFrame({
+        "type": [round(random.uniform(0, 1))],
+        "fixed_acidity": [random.uniform(fixed_acidity_max, fixed_acidity_min)],
+        "volatile_acidity": [random.uniform(volatile_acidity_max, volatile_acidity_min)],
+        "citric_acid": [random.uniform(citric_acid_max, citric_acid_min)],
+        "residual_sugar": [random.uniform(residual_sugar_max, residual_sugar_min)],
+        "chlorides": [random.uniform(chlorides_max, chlorides_min)],
+        "free_sulfur_dioxide": [random.uniform(free_sulfur_dioxide_max, free_sulfur_dioxide_min)],
+        "total_sulfur_dioxide": [random.uniform(total_sulfur_dioxide_max, total_sulfur_dioxide_min)],
+        "density": [random.uniform(density_max, density_min)],
+        "pH": [random.uniform(pH_max, pH_min)],
+        "sulphates": [random.uniform(sulphates_max, sulphates_min)],
+        "alcohol": [random.uniform(alcohol_max, alcohol_min)]
+    })
+    df['quality'] = name
     return df
 
-
-def get_random_iris_flower():
+def get_random_wine():
     """
-    Returns a DataFrame containing one random iris flower
+    Returns a DataFrame containing one random wine
     """
     import pandas as pd
     import random
 
-    virginica_df = generate_flower("Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
-    versicolor_df = generate_flower("Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
-    setosa_df =  generate_flower("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
+    quality3_df = generate_wine("3", 11.8, 4.2, 1.58, 0.17, 0.66, 0.0, 16.2, 0.7, 0.267, 0.022, 289.0, 3.0, 440.0, 9.0, 1.0, 0.99, 3.63, 2.87, 0.86, 0.28, 12.6, 8.0)
+    quality4_df = generate_wine("4", 12.5, 4.6, 1.13, 0.11, 1.0, 0.0, 17.55, 0.7, 0.61, 0.013, 138.5, 3.0, 272.0, 7.0, 1.0, 0.99, 3.90, 2.74, 2.0, 0.25, 13.5, 8.4)
+    quality5_df =  generate_wine("5", 11.8, 4.2, 1.58, 0.17, 0.66, 0.0, 16.2, 0.7, 0.267, 0.022, 289.0, 3.0, 440.0, 9.0, 1.0, 0.99, 3.63, 2.87, 0.86, 0.28, 12.6, 8.0)
+    quality6_df = generate_wine("6", 11.8, 4.2, 1.58, 0.17, 0.66, 0.0, 16.2, 0.7, 0.267, 0.022, 289.0, 3.0, 440.0, 9.0, 1.0, 0.99, 3.63, 2.87, 0.86, 0.28, 12.6, 8.0)
+    quality7_df = generate_wine("7", 11.8, 4.2, 1.58, 0.17, 0.66, 0.0, 16.2, 0.7, 0.267, 0.022, 289.0, 3.0, 440.0, 9.0, 1.0, 0.99, 3.63, 2.87, 0.86, 0.28, 12.6, 8.0)
+    quality8_df =  generate_wine("8", 11.8, 4.2, 1.58, 0.17, 0.66, 0.0, 16.2, 0.7, 0.267, 0.022, 289.0, 3.0, 440.0, 9.0, 1.0, 0.99, 3.63, 2.87, 0.86, 0.28, 12.6, 8.0)
+    quality9_df = generate_wine("9", 11.8, 4.2, 1.58, 0.17, 0.66, 0.0, 16.2, 0.7, 0.267, 0.022, 289.0, 3.0, 440.0, 9.0, 1.0, 0.99, 3.63, 2.87, 0.86, 0.28, 12.6, 8.0)
 
-    # randomly pick one of these 3 and write it to the featurestore
-    pick_random = random.uniform(0,3)
-    if pick_random >= 2:
-        iris_df = virginica_df
-        print("Virginica added")
-    elif pick_random >= 1:
-        iris_df = versicolor_df
-        print("Versicolor added")
-    else:
-        iris_df = setosa_df
-        print("Setosa added")
-
-    return iris_df
+    wines = [quality3, quality4, quality5, quality6, quality7, quality8, quaility9]
+    # randomly pick one of these 7 and write it to the featurestore
+    pick_random = round(random.uniform(0,7))
+    wine_df = wines[pick_random]
+    print(f'Wine with Quality wine_df["quality"] added')
+    
+   return wine_df
 
 
 def g():
@@ -62,15 +71,15 @@ def g():
     project = hopsworks.login()
     fs = project.get_feature_store()
 
-    iris_df = get_random_iris_flower()
+    wine_df = get_random_iris_flower()
 
-    iris_fg = fs.get_feature_group(name="iris",version=1)
-    iris_fg.insert(iris_df)
+    wine_fg = fs.get_feature_group(name="wine",version=1)
+    wine_fg.insert(wine_df)
 
 if __name__ == "__main__":
     if LOCAL == True :
         g()
     else:
-        stub.deploy("iris_daily")
+        stub.deploy("wine_daily")
         with stub.run():
             f()
