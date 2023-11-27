cd2926ffbc9009360eab521d8fa247cfaa5ad358
aym1king <49128030+aym1king@users.noreply.github.com>
2023-11-27 07:35:12 +0100

    Update app.py

--- wine/huggingface-spaces-wine-monitor/app.py
+++ wine/huggingface-spaces-wine-monitor/app.py
@@ -7,25 +7,25 @@ fs = project.get_feature_store()
 
 dataset_api = project.get_dataset_api()
 
-dataset_api.download("Resources/images/latest_iris.png")
-dataset_api.download("Resources/images/actual_iris.png")
+dataset_api.download("Resources/images/latest_wine.png")
+dataset_api.download("Resources/images/actual_wine.png")
 dataset_api.download("Resources/images/df_recent.png")
-dataset_api.download("Resources/images/confusion_matrix.png")
+dataset_api.download("Resources/images/wine_confusion_matrix.png")
 
 with gr.Blocks() as demo:
     with gr.Row():
       with gr.Column():
           gr.Label("Today's Predicted Image")
-          input_img = gr.Image("latest_iris.png", elem_id="predicted-img")
+          input_img = gr.Image("latest_wine.png", elem_id="predicted-img")
       with gr.Column():          
           gr.Label("Today's Actual Image")
-          input_img = gr.Image("actual_iris.png", elem_id="actual-img")        
+          input_img = gr.Image("actual_wine.png", elem_id="actual-img")        
     with gr.Row():
       with gr.Column():
           gr.Label("Recent Prediction History")
           input_img = gr.Image("df_recent.png", elem_id="recent-predictions")
       with gr.Column():          
           gr.Label("Confusion Maxtrix with Historical Prediction Performance")
-          input_img = gr.Image("confusion_matrix.png", elem_id="confusion-matrix")        
+          input_img = gr.Image("wine_confusion_matrix.png", elem_id="confusion-matrix")        
 
 demo.launch()
