11117a32a1ad03b1229dcaa1fb7f27471e476535
aym1king <49128030+aym1king@users.noreply.github.com>
2023-11-27 07:34:11 +0100

    Update app.py

--- wine/huggingface-spaces-wine/app.py
+++ wine/huggingface-spaces-wine/app.py
@@ -4,44 +4,58 @@ import requests
 import hopsworks
 import joblib
 import pandas as pd
+import numpy as np
 
 project = hopsworks.login()
 fs = project.get_feature_store()
 
 
 mr = project.get_model_registry()
-model = mr.get_model("iris_model", version=1)
+model = mr.get_model("wine_model", version=1)
 model_dir = model.download()
-model = joblib.load(model_dir + "/iris_model.pkl")
+model = joblib.load(model_dir + "/wine_model.pkl")
 print("Model downloaded")
 
-def iris(sepal_length, sepal_width, petal_length, petal_width):
+def wine(type, fixed acidity, volatile acidity, citric acid, residual sugar, chlorides, free sulfur dioxide, total sulfur dioxide, density, pH, sulphates, alcohol):
     print("Calling function")
 #     df = pd.DataFrame([[sepal_length],[sepal_width],[petal_length],[petal_width]], 
-    df = pd.DataFrame([[sepal_length,sepal_width,petal_length,petal_width]], 
-                      columns=['sepal_length','sepal_width','petal_length','petal_width'])
+    if type == "red" or type == "RED" or type == "Red":
+        type = 1
+    elif type == "white" or type=="WHITE" or type == "White":
+        type = 0
+        
+    df = pd.DataFrame([[type, fixed acidity, volatile acidity, citric acid, residual sugar, chlorides, free sulfur dioxide, total sulfur dioxide, density, pH, sulphates, alcohol]], 
+                      columns=['type', 'fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol'])
     print("Predicting")
     print(df)
     # 'res' is a list of predictions returned as the label.
-    res = model.predict(df) 
+    res = np.round(model.predict(df)) 
     # We add '[0]' to the result of the transformed 'res', because 'res' is a list, and we only want 
     # the first element.
 #     print("Res: {0}").format(res)
     print(res)
-    flower_url = "https://raw.githubusercontent.com/featurestoreorg/serverless-ml-course/main/src/01-module/assets/" + res[0] + ".png"
-    img = Image.open(requests.get(flower_url, stream=True).raw)            
+    wine_url = "https://raw.githubusercontent.com/aym1king/serverless-intro/master/wine/wine_imgs/" + res[0] + ".png"
+    img = Image.open(requests.get(wine_url, stream=True).raw)            
     return img
         
 demo = gr.Interface(
-    fn=iris,
-    title="Iris Flower Predictive Analytics",
-    description="Experiment with sepal/petal lengths/widths to predict which flower it is.",
+    fn=wine,
+    title="Wine Quality Predictive Analytics",
+    description="Experiment with wine features to predict which quality it has.",
     allow_flagging="never",
     inputs=[
-        gr.inputs.Number(default=2.0, label="sepal length (cm)"),
-        gr.inputs.Number(default=1.0, label="sepal width (cm)"),
-        gr.inputs.Number(default=2.0, label="petal length (cm)"),
-        gr.inputs.Number(default=1.0, label="petal width (cm)"),
+        gr.inputs.Number(default="white", label="type (white or red)"),
+        gr.inputs.Number(default=7.3, label="fixed acidity"),
+        gr.inputs.Number(default=0.4, label="volatile acidity"),
+        gr.inputs.Number(default=0.3, label="citric acid"),
+        gr.inputs.Number(default=5.8, label="residual sugar"),
+        gr.inputs.Number(default=0.1, label="chlorides"),
+        gr.inputs.Number(default=30, label="free sulfur dioxide"),
+        gr.inputs.Number(default=120, label="total sulfur dioxide"),
+        gr.inputs.Number(default=1.0, label="density"),
+        gr.inputs.Number(default=3.2, label="pH"),
+        gr.inputs.Number(default=0.5, label="sulphates"),
+        gr.inputs.Number(default=9.8, label="alcohol"),
         ],
     outputs=gr.Image(type="pil"))
 
