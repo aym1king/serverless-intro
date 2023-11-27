48e264cf8267fb58fc5b9fdf616055656f5f571e
aym1king <49128030+aym1king@users.noreply.github.com>
2023-11-27 22:18:41 +0100

    Update wine-feature-pipeline-daily.py

--- wine/wine-feature-pipeline-daily.py
+++ wine/wine-feature-pipeline-daily.py
@@ -57,9 +57,8 @@ def get_random_wine():
 
     wines = [quality3_df, quality4_df, quality5_df, quality6_df, quality7_df, quality8_df, quality9_df]
     # randomly pick one of these 7 and write it to the featurestore
-    #pick_random = round(random.uniform(0,7))
-    #wine_df = wines[pick_random]
-    wine_df = quality8_df
+    pick_random = round(random.uniform(-0.49,7.49))
+    wine_df = wines[pick_random]
     print(f'Wine with Quality wine_df["quality"] added')
     
     return wine_df
