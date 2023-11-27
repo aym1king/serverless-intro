6877076948c9a48cba435991a77471ff49478da6
aym1king <49128030+aym1king@users.noreply.github.com>
2023-11-27 21:52:16 +0100

    Update wine-batch-inference-pipeline.py

--- wine/wine-batch-inference-pipeline.py
+++ wine/wine-batch-inference-pipeline.py
@@ -39,6 +39,7 @@ def g():
     #print(y_pred)
     offset = 4
     wine = int(y_pred[y_pred.size-offset])
+    wine = 3
     wine_url = "https://raw.githubusercontent.com/aym1king/serverless-intro/main/wine/wine_imgs/" + str(wine) + ".png"
     print(wine_url)
     print("Wine quality predicted: " + str(wine))
@@ -51,6 +52,7 @@ def g():
     df = wine_fg.read() 
     #print(df)
     label = int(df.iloc[-offset]["quality"])
+    label = 3
     label_url = "https://raw.githubusercontent.com/aym1king/serverless-intro/main/wine/wine_imgs/" + str(label) + ".png"
     print("Wine quality actual: " + str(label))
     img = Image.open(requests.get(label_url, stream=True).raw)            
